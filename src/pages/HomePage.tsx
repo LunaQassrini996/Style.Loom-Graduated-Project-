1acaa8cbb03f9ee16c7fef7ba067cdba55b276e7
mahayoub123 <mahmoudayoub180699@gmail.com>
2025-08-09 18:15:48 +0300

    Testimonials section and updates

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
-import FaqQuestions from "../components/FaqQuestions";
+import FaqQuestions from "../Components/FaqQuestions";
+import Testimonials from "../Components/Testimonials/Testimonials";
 
 const HomePage = () => {
   return (
     <div>
       <FaqQuestions />
+      <Testimonials />
     </div>
   );
 };
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import faqReducer from "../slices/faqSlice";
+import testimonialsReducer from "../slices/testimonialsSlice"; // تأكد من وجود هذا
 
 export const store = configureStore({
   reducer: {
     faqs: faqReducer,
+    testimonials: testimonialsReducer,
   },
 });
 
